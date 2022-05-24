c879e45def975ca4701be44895989e3a3cce1623
Charles Clark <charles.clark@stonybrook.edu>
2022-05-24 16:19:46 -0400

    fixed crop range and out_path

--- crop_tomo.py
+++ crop_tomo.py
@@ -2,7 +2,7 @@
 File: read_tomo
 Name: Charles Clark
 -------------------
-Reads in 3D tomography data and crop to desired range (x, y and z)
+Reads in 3D tomography data and crop to desired range (x, y, and z)
 """
 import numpy as np
 import h5py
@@ -10,20 +10,25 @@ from skimage import io, exposure
 import matplotlib.pyplot as plt
 from os.path import exists
 
-in_path = 'E:/XIAOYANG_Proposal_307818/recon_scan_'
-out_path = 'EuCl3_in-situ/20210709/cropped_charles'
+in_path = 'H:/XIAOYANG_Proposal_307818/'
+out_path = 'H:/XIAOYANG_Proposal_307818/cropped_charles/'
+
+x_min, x_max = 300, 1000
+y_min, y_max = 300, 1000
+z_min, z_max = 300, 700
 
 # loop over scans to be processed
 for i in range(99925, 100010+1):
     scan_id = str(i)
-    fn = in_path + scan_id + '_bin1.h5'
+    fn = in_path + 'recon_scan_' + scan_id + '_bin1.h5'
     
     # check that reconstructed scan exists
     if exists(fn):
         file = h5py.File(fn, 'r')  # load .h5 file
                 
-        img = file.get('img')[300:700, 300:1000, 300:1000]  # get 500th slice of 3D image
+        # crop scan
+        img = file.get('img')[z_min:z_max, y_min:y_max, x_min:x_max] 
         img = np.array(img)  # convert to numpy array (of floats)
 
-        io.imsave(out_path + 'recon' + scan_id + 'cropped.tif', img)
+        io.imsave(out_path + 'recon_' + scan_id + '_cropped.tif', img)
 
