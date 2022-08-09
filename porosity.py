73b24889377dbfd24e6f3cbd444f38ce50c1258f
Charles Clark <charles.clark@stonybrook.edu>
2022-08-09 15:52:56 -0400

    crop data

--- porosity.py
+++ porosity.py
@@ -14,6 +14,14 @@ IN_PATH = r'/media/karenchen-wiegart/20210321_FXI_backup/20210321_FXI_Backup/cro
 os.chdir(IN_PATH)
 
 
+def crop3d(im, crop_size=100):
+    """
+    crops 3d image to middle number of slices specified 
+    """
+    mid_pt = int(im.shape[0] / 2)
+    return im[mid_pt - int(crop_size/2): mid_pt + int(crop_size/2)]
+
+
 if __name__ == '__main__':
     files = sorted(glob('*.tiff'))
     
@@ -28,10 +36,12 @@ if __name__ == '__main__':
         
         im3d = io.imread(fn)  # cropped and masked image
         
+        im3d = crop3d(im3d, crop_size=400)
+        
         material = np.count_nonzero(im3d==2)
         pores = np.count_nonzero(im3d==1)
         porosity[i] = pores / (material + pores)
         
     scan_porosity = np.vstack((scan, porosity)).T
-    np.savetxt('porosities.txt', scan_porosity, delimiter=', ', header='scan, porosity')
+    np.savetxt('porosities.txt', scan_porosity, fmt=['%d','%.18e'], delimiter=', ', header='scan, porosity')
     
