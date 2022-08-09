53a69b2d63380a34dafac6140771dce3110a7ecf
Charles Clark <charles.clark@stonybrook.edu>
2022-08-09 16:46:31 -0400

    save mass loss

--- mass_loss.py
+++ mass_loss.py
@@ -7,22 +7,29 @@ Created on Tue Jul 19 14:38:34 2022
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io
-from os import listdir
+from os import listdir, chdir
 import segmentation
+from glob import glob
 
-IN_PATH = r'/media/karenchen-wiegart/20210321_FXI_backup/20210321_FXI_Backup/cropped_new_segmentation'
-
+IN_PATH = r'E:/20210321_FXI_Backup/Charles/cropped_new_segmentation'
+OUT_PATH = r'E:/20210321_FXI_Backup/Charles'
+chdir(IN_PATH)
 
 if __name__ == '__main__':
     
-    files = sorted(listdir(IN_PATH))
+    files = sorted(glob('*.tif'))
     
     masses = np.zeros(len(files))
+    scans = np.zeros(len(files))
     
     # assumes scans are in sequential order in folder
-    for i, scan in enumerate(files):
-        im3d = io.imread(f'{IN_PATH}/{scan}')
-        im3d = segmentation.crop3d(im3d, crop_size=2)
+    for i, fn in enumerate(files):
+        scan_id = [int(s) for s in fn.split('_') if s.isdigit()][0]
+        
+        scans[i] = scan_id
+        
+        im3d = io.imread(f'{IN_PATH}/{fn}')
+        im3d = segmentation.crop3d(im3d, crop_size=400)
                 
         # seg_im = segmentation.seg_3d(im3d, 'Gaussian', 3, crop_im=(True), plot_seg=(True))
         masses[i] = np.count_nonzero(im3d)
@@ -30,5 +37,11 @@ if __name__ == '__main__':
     print(masses)
     
     rel_masses = masses / masses[0]
-    plt.plot(rel_masses)
+    plt.plot(rel_masses, 'o')
     plt.show()
+    
+    scans_mass = np.column_stack((scans, masses, rel_masses))
+    np.savetxt(f'{OUT_PATH}/mass_loss.txt', scans_mass, fmt=['%d','%d','%.18e'], delimiter=', ', header='scan, mass, rel_mass')
+    
+    
+    
