d30edb965551071e677ee49899ff322097ae596b
Charles Clark <charles.clark@stonybrook.edu>
2022-09-13 16:17:01 -0400

    mass -> vol

--- plot_vol-loss_porosity.py
+++ plot_vol-loss_porosity.py
@@ -10,59 +10,56 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #%% load data
-IN_PATH = 'C:/Users/clark/OneDrive - Stony Brook University/Documents/Karen/Molten Salt/EuCl3_in-situ'
-OUT_PATH = 'C:/Users/clark/OneDrive - Stony Brook University/Documents/Karen/Molten Salt/EuCl3_in-situ/mass-loss_porosity_plots'
-fn1 = '20210321_porosity_mass-loss.xlsx'
+IN_PATH = 'C:/Users/clark/OneDrive - Stony Brook University/Documents/Karen/Molten Salt/EuCl3_in-situ/results'
+OUT_PATH = 'C:/Users/clark/OneDrive - Stony Brook University/Documents/Karen/Molten Salt/EuCl3_in-situ/vol-loss_porosity_plots'
+fn1 = '20210321_porosity_vol-loss_trimmed.xlsx'
 df1 = pd.read_excel(f'{IN_PATH}/{fn1}')
 
-fn2 = '20210709_porosity_mass-loss.xlsx'
+fn2 = '20210709_porosity_vol-loss_trimmed.xlsx'
 df2 = pd.read_excel(f'{IN_PATH}/{fn2}')
-df2 = df2.iloc[1:, :]  # drop scan 99925
 
 #%% organize data
 time_5050 = df1['time']
 porosity_5050 = df1['porosity']
-# rel_mass = df['rel_mass']
-mass_5050 = np.array(df1['mass'])
-rel_mass_5050 = mass_5050 / mass_5050[0]
+rel_vol_5050 = df1['rel_vol']
+vol_5050 = np.array(df1['vol'])
 
 time_eutectic = df2['time']
 porosity_eutectic = df2['porosity']
-# rel_mass = df['rel_mass']
-mass_eutectic = np.array(df2['mass'])
-rel_mass_eutectic = mass_eutectic / mass_eutectic[0]
+rel_vol_eutectic = df2['rel_vol']
+vol_eutectic = np.array(df2['vol'])
 
 #%% set plot parameters
 plt.rc('axes', labelsize=18, titlesize=21)
 plt.rc('xtick', labelsize=15)
 plt.rc('ytick', labelsize=15)
 
-#%% plot mass-loss
+#%% plot vol-loss
 plt.rc('lines', markersize=12)
 
 plt.figure(figsize=[10,8])
 plt.title('50:50 $KCl$-$MgCl_2$, 1wt.% $EuCl_3$')
 plt.xlabel('Time (min)')
-plt.ylabel('Mass Remaining (%)')
-plt.plot(time_5050, rel_mass_5050*100, '.', c='r')
-plt.savefig(f'{OUT_PATH}/50-50_mass-loss.tif', format='tif')
+plt.ylabel('Vol Remaining (%)')
+plt.plot(time_5050, rel_vol_5050*100, '.', c='r')
+plt.savefig(f'{OUT_PATH}/50-50_vol-loss.tif', format='tif')
 plt.show()
 
 plt.figure(figsize=[10,8])
 plt.title('Eutectic $KCl$-$MgCl_2$, 1wt.% $EuCl_3$')
 plt.xlabel('Time (min)')
-plt.ylabel('Mass Remaining (%)')
-plt.plot(time_eutectic, rel_mass_eutectic*100, '.', c='k')
-plt.savefig(f'{OUT_PATH}/Eutectic_mass-loss.tif', format='tif')
+plt.ylabel('Vol Remaining (%)')
+plt.plot(time_eutectic, rel_vol_eutectic*100, '.', c='k')
+plt.savefig(f'{OUT_PATH}/Eutectic_vol-loss.tif', format='tif')
 plt.show()
 
 plt.figure(figsize=[10,8])
 plt.xlabel('Time (min)')
-plt.ylabel('Mass Remaining (%)')
-plt.plot(time_5050, rel_mass_5050*100, '.', c='r', label='50:50 $KCl$-$MgCl_2$')
-plt.plot(time_eutectic, rel_mass_eutectic*100, '.', c='k', label='eutectic $KCl$-$MgCl_2$')
+plt.ylabel('Vol Remaining (%)')
+plt.plot(time_5050, rel_vol_5050*100, '.', c='r', label='50:50 $KCl$-$MgCl_2$')
+plt.plot(time_eutectic, rel_vol_eutectic*100, '.', c='k', label='eutectic $KCl$-$MgCl_2$')
 plt.legend(fontsize=15)
-plt.savefig(f'{OUT_PATH}/comparison_mass-loss.tif', format='tif')
+plt.savefig(f'{OUT_PATH}/comparison_vol-loss.tif', format='tif')
 plt.show()
 #%% plot porosity
 plt.rc('lines', markersize=10, markeredgewidth=2)
